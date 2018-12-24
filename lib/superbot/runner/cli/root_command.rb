fb1c36d3006120af115ae38c51078799762b8467
DeeMak13 <dima.m@active-bridge.com>
2018-12-24 13:17:41 +0200

    Support run for both directories and separate files, add --no-teleport flag

--- lib/superbot/runner/cli/root_command.rb
+++ lib/superbot/runner/cli/root_command.rb
@@ -5,7 +5,7 @@ module Superbot
         include Superbot::Validations
         include Superbot::Cloud::Validations
 
-        parameter "RECORDING", "recording"
+        parameter "PATH", "path to test suite"
 
         option ['--browser'], 'BROWSER', "Browser type to use. Can be either local or cloud", default: 'local' do |browser|
           validates_browser_type browser
@@ -13,17 +13,13 @@ module Superbot
           browser
         end
         option ['--region'], 'REGION', 'Region for remote webdriver'
+        option ['--no-teleport'], :flag, 'Do not start teleport before running'
 
         def execute
-          open_teleport
-
-          file_extension = File.extname(recording)
-          case file_extension
-          when '.side' then Superbot::Runner::Side.run(recording)
-          else abort "#{file_extension} scripts are not supported yet"
-          end
+          open_teleport unless no_teleport?
+          sorted_files.each(&method(:run_test_file))
         ensure
-          close_teleport
+          close_teleport unless no_teleport?
         end
 
         private
@@ -37,6 +33,26 @@ module Superbot
             chromedriver_path = Chromedriver::Helper.new.binary_path
             @chromedriver = Kommando.new "#{chromedriver_path} --silent --port=9515 --url-base=wd/hub"
             @chromedriver.run_async
+          end
+        end
+
+        def sorted_files
+          if File.directory?(path)
+            Dir.glob(File.join(path, '*'))
+               .map! { |f| [f.gsub(path, ''), f] }
+               .sort
+               .map(&:last)
+           else
+             [path]
+           end
+        end
+
+        def run_test_file(test_file)
+          file_extension = File.extname(test_file)
+          puts "Running #{test_file}"
+          case file_extension
+          when '.side' then Superbot::Runner::Side.run(test_file)
+          else puts "#{file_extension} scripts are not supported yet"
           end
         end
 
